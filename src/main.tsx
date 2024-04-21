216b95056754a812f58213253b38e239819a9461
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-04-21 12:21:30 +0500

    feat(created: Theme Mode)

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@/components/theme-provider.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -5,6 +6,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 )
